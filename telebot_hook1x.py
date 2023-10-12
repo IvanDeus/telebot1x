01eec712d53da07e648f847cece122c6a7f4ba10
IvanDeus <146208093+IvanDeus@users.noreply.github.com>
2023-10-12 20:38:53 +0300

    Update telebot_hook1x.py
    Chat with manager function added

--- telebot_hook1x.py
+++ telebot_hook1x.py
@@ -1,5 +1,5 @@
 ## telegram bot
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for, make_response, abort
 import json
 import requests
 import pymysql
@@ -121,6 +121,106 @@ def find_subbed_chatids(conn):
         return []
 
 
+
+def change_step_status(chat_id, conn, stp):
+    try:
+        with conn.cursor() as cursor:
+            # Use parameterized query to update the subscription status
+            query = "UPDATE telebot_users SET step = %s WHERE chat_id = %s"
+            cursor.execute(query, (stp, chat_id))
+            # Commit the changes to the database
+            conn.commit()
+    except pymysql.Error as e:
+        # Handle any database errors here
+        print(f"Database error: {e}")
+
+def update_chat_table(conn, uchat_id, mngmsg):
+    try:
+        with conn.cursor() as cursor:
+            mngmsg = conn.escape_string(mngmsg)
+             # Check if the user already exists in the table
+            query = f"SELECT id FROM telebot_chats WHERE uchat_id = '{uchat_id}' ORDER BY lastupd DESC LIMIT 1"
+            cursor.execute(query)
+            result_id = cursor.fetchone()
+              # Proceed to update
+            if result_id:
+               mngmsg = " : "+mngmsg
+             # User exists, update the record
+               update_query = "UPDATE telebot_chats SET mngmsg = CONCAT(mngmsg, %s) WHERE id = %s"
+               cursor.execute(update_query, (mngmsg, result_id[0]))
+               conn.commit()
+            else:
+               print("no chat was found by manager")
+
+    except pymysql.Error as e:
+        # Handle any database errors here
+        print(f"Database error: {e}")
+
+def insert_into_chat_table(conn, uchat_id, mngchat_id, umsg, mngmsg):
+    try:
+        with conn.cursor() as cursor:
+            # Use parameterized query to update the subscription status
+            query = "Insert Into telebot_chats SET uchat_id = %s, mngchat_id = %s, umsg = %s, mngmsg = %s"
+            cursor.execute(query, (uchat_id, mngchat_id, umsg, mngmsg))
+            # Commit the changes to the database
+            conn.commit()
+    except pymysql.Error as e:
+        # Handle any database errors here
+        print(f"Database error: {e}")
+
+def find_admin_chatid(conn, adm_name):
+    try:
+        with conn.cursor() as cursor:
+            query = "SELECT chat_id FROM telebot_admins WHERE name = %s LIMIT 1"
+            cursor.execute(query, (adm_name,))
+            result = cursor.fetchone()
+            if result:
+                return result[0]  # Return the chat_id if found
+            else:
+                return None  # Return None if admin user not found
+    except Exception as e:
+        # Handle any exceptions (e.g., database connection error)
+        print(f"Error: {e}")
+        return None
+
+def get_user_chats_table(conn):
+    try:
+        with conn.cursor() as cursor:
+             query = ("SELECT tc.id AS telebot_chat_id, tc.lastupd AS telebot_chat_lastupd,"
+          " tc.uchat_id, tu.name AS user_name, tu.first_name AS user_frstname, tc.umsg, ta.name AS admin_name, tc.mngmsg"
+          " FROM telebot_chats tc JOIN telebot_users tu ON tc.uchat_id = tu.chat_id"
+          " JOIN telebot_admins ta ON tc.mngchat_id = ta.chat_id Limit 100")
+             cursor.execute(query)
+             results = cursor.fetchall()
+        return results
+    except Exception as e:
+        # Handle any exceptions (e.g., database connection error)
+        print(f"Error: {e}")
+        return []
+
+def get_step_status(chat_id, conn):
+    try:
+        with conn.cursor() as cursor:
+            # Use parameterized query to select the step status
+            query = "SELECT step FROM telebot_users WHERE chat_id = %s"
+            cursor.execute(query, (chat_id,))
+            result = cursor.fetchone()
+            if result:
+                return result[0]  # Return the step status if found
+            else:
+                return None  # Return None if user not found
+    except pymysql.Error as e:
+        # Handle any database errors here
+        print(f"Database error: {e}")
+        return None
+
+
+
+
+
+
+
+
 @bot.message_handler()
 def handle_nostart(message):
     chat_id = message.chat.id
@@ -148,18 +248,21 @@ def handle_start(message):
     # Send a message with the inline keyboard
     bot.send_message(chat_id, msg1, reply_markup=keyboard)
 
+###### Dictionary to store active chats initiated by users
+manager_chat_id = '750334690'
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback(call, conn):
     keyboard2 = types.InlineKeyboardMarkup()
     button1 = types.InlineKeyboardButton('Guide', callback_data='/guide')
     button3 = types.InlineKeyboardButton('Sub', callback_data='/sub')
     button4 = types.InlineKeyboardButton('Unsub', callback_data='/unsub')
+    button5 = types.InlineKeyboardButton('Call Manager', callback_data='/manager')
     # Add the buttons to the keyboard
-    keyboard2.add(button1, button3, button4)
-
+    keyboard2.add(button1, button3, button4, button5)
     # Handle button presses
     chat_id = call.message.chat.id
     data = call.data
+    user_name = call.message.chat.username
     msg1 = ("Super guide! Enjoy!")
     msg2 = ("This is a help message. Try /start or 'guide'. "
             "You can subscribe with 'sub' and undo with 'unsub'")
@@ -180,12 +283,77 @@ def handle_callback(call, conn):
         sub = 0
         change_sub_status(chat_id, conn, sub)
         bot.send_message(chat_id, "You unsubscribed")
-
+    elif data == '/manager':
+     # Notify the manager
+        bot.send_message(manager_chat_id, f"{chat_id}: {user_name}: has initiated a chat. Respond via the bot.")
+        change_step_status(chat_id, conn, 101)
+        insert_into_chat_table(conn, chat_id, manager_chat_id, "Call manager", " ")
 
 # Telegram bot
 @app.route('/')
 def hello():
     return render_template('hello.html')
+
+
+
+# chat login route
+@app.route('/admin-chat', methods=['GET', 'POST'])
+def login_chat():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # get admin chat id from a db for auth
+        conn = pymysql.connect(unix_socket=mysql_unix_socket, user=db_username, password=db_password, database=db_name)
+        admin_chatid = find_admin_chatid(conn,username)
+        # Close the database connection
+        conn.close()
+        # Check if the provided admin credentials are correct
+        if admin_chatid and password == db_password:
+            try:
+                # Create a response object and set cookies
+                response = make_response(redirect(url_for('chat_page')))  # Redirect to admin_page
+                # check admin cookie
+                response.set_cookie('chat_cookie_id', '{admin_chatid}cookie_chat_passed_tst1212')
+                response.set_cookie('chat_cookie_name', '{username}')
+                return response  # Redirect to the admin page
+
+            except Exception as e:
+                # Handle any exceptions here
+                print(f"Error: {e}")
+                return "An error occurred."
+        else:
+            return "Invalid admin credentials. Please try again."
+
+    return render_template('login_chat.html')
+
+
+
+@app.route('/chat_page', methods=['GET'])
+def chat_page():
+    # Check if the admin_cookie_id is set
+    admin_cookie_id = request.cookies.get('chat_cookie_id')
+    admin_cookie_name = request.cookies.get('chat_cookie_name')
+    # Create a database connection with Unix socket
+    conn = pymysql.connect(unix_socket=mysql_unix_socket, user=db_username, password=db_password, database=db_name)
+    # get admin chat id from cookie name
+    admin_chatid = find_admin_chatid(conn,admin_cookie_name)
+    if admin_cookie_id != '{admin_chatid}cookie_chat_passed_tst1212':  # cookie check
+        abort(403)  # Return a forbidden error if the cookie is not set
+    try:
+        # get table
+        users = get_user_chats_table(conn)
+         # create user table
+        return render_template('chat_table.html', users=users)
+
+    except Exception as e:
+        # Handle any exceptions here
+        print(f"Error: {e}")
+        return "An error occurred."
+    # Close the database connection
+    conn.close()
+
+
+
 
 
 # Main logic
@@ -199,7 +367,7 @@ def telebothook1x():
         json_string = request.get_data().decode('UTF-8')
         update = telebot.types.Update.de_json(json_string)
         # Analyze recived message
-        ##print (update.message)
+       ## print (update)
         if (update.message is not None) and (update.message.text is not None):
             message = update.message
             # Access name and chat_id only when there is a message
@@ -214,6 +382,7 @@ def telebothook1x():
             chat_id = message.chat.id
              # Call your add_or_update_user function to add/update the user in the database
             add_or_update_user(chat_id, name, message.text, conn, first_name, last_name)
+            user_step_stat = get_step_status(chat_id, conn)
             # Handle start command
             if message.text == '/start':
                 handle_start(message)
@@ -224,12 +393,30 @@ def telebothook1x():
                  # Find all subscribed chat IDs
                 subscribed_chat_ids = find_subbed_chatids(conn)
                 message_text = message.text[len('/forward '):] #cut command forward
-                if message_text: 
+                if message_text:
                     # Loop through the subscribed chat IDs and send the message
                    for uchat_id in subscribed_chat_ids:
                        #bot.forward_message(uchat_id, chat_id, message.id)
                        bot.send_message(uchat_id, message_text)
                        time.sleep(1)  # Add a delay
+            elif user_step_stat == 101:
+                # forward user who called manager
+                fwd_umsg = str(chat_id) +": "+first_name+": "+ message.text
+                bot.send_message(manager_chat_id, fwd_umsg)
+                insert_into_chat_table(conn, chat_id, manager_chat_id, fwd_umsg, 'NOT REPLIED')
+            elif chat_id == int(manager_chat_id):
+                # extract user chat id from replied msg
+                if message.reply_to_message is not None:
+                   reply_text = message.reply_to_message.text
+                   user_chat_id = reply_text.split(': ')
+                   if message.text == '/end':
+                      change_step_status(user_chat_id[0], conn, 0)
+                   else:
+                   # admin sends a message to a user who requested mmngr
+                      bot.send_message(user_chat_id[0], "Manager: "+ message.text)
+                   update_chat_table(conn, user_chat_id[0], message.text)
+                else:
+                   bot.send_message(chat_id, "No message is sent, try 'reply'. To stop chat with user reply /end")
             else:
                 handle_nostart(message) # all others imput
 
