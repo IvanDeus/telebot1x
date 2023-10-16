7eb9713beccdc5549d7a0fcec84bb73b58efc883
IvanDeus <146208093+IvanDeus@users.noreply.github.com>
2023-10-16 16:34:45 +0300

    Update telebot_hook1x_admin_password.py

--- telebot_hook1x_admin_password.py
+++ telebot_hook1x_admin_password.py
@@ -1,50 +1,50 @@
-import argparse
-import bcrypt
+import sys
 import pymysql
+from passlib.hash import bcrypt
 
-# Import your database credentials
-from telebot_hook1x_cfg import db_host, db_username, db_password, db_name, mysql_unix_socket
+# Database credentials
+db_host = "your_db_host"
+db_username = "your_db_username"
+db_password = "your_db_password"
+db_name = "your_db_name"
 
-# Parse command-line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("username", help="Username to insert or update")
-parser.add_argument("password", help="Password to hash and store")
-args = parser.parse_args()
+# Function to create a database connection
+def create_db_connection():
+    return pymysql.connect(
+        host=db_host,
+        user=db_username,
+        password=db_password,
+        db=db_name
+    )
 
-# Define a function to securely hash a password
-def hash_password(password):
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+# Function to store or update a user's password
+def store_or_update_password(username, password):
+    conn = create_db_connection()
+    cursor = conn.cursor()
 
-# Connect to the database
-connection = pymysql.connect(
-    host=db_host,
-    user=db_username,
-    password=db_password,
-    db=db_name,
-    unix_socket=mysql_unix_socket
-)
+    # Check if the user already exists in the table
+    cursor.execute("SELECT * FROM telebot_admins WHERE name = %s", (username,))
+    user_data = cursor.fetchone()
 
-try:
-    with connection.cursor() as cursor:
-        # Check if the username exists
-        cursor.execute("SELECT * FROM telebot_admins WHERE name = %s", (args.username,))
-        existing_user = cursor.fetchone()
+    if user_data:
+        # User exists, update the password
+        hashed_password = bcrypt.using(schemes=['bcrypt']).hash(password)
+        cursor.execute("UPDATE telebot_admins SET passwd = %s WHERE name = %s", (hashed_password, username))
+    else:
+        # User doesn't exist, insert a new record
+        hashed_password = bcrypt.using(schemes=['bcrypt']).hash(password)
+        cursor.execute("INSERT INTO telebot_admins (name, passwd) VALUES (%s, %s)", (username, hashed_password))
 
-        # Hash the provided password
-        hashed_password = hash_password(args.password)
+    conn.commit()
+    conn.close()
 
-        if existing_user:
-            # Update the existing user
-            cursor.execute("UPDATE telebot_admins SET passwd = %s WHERE name = %s", (hashed_password, args.username))
-            connection.commit()
-            print(f"Password for {args.username} updated successfully.")
-        else:
-            # Insert a new user
-            cursor.execute("INSERT INTO telebot_admins (name, passwd) VALUES (%s, %s)", (args.username, hashed_password))
-            connection.commit()
-            print(f"New user {args.username} inserted successfully.")
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <username> <password>")
+        sys.exit(1)
 
-finally:
-    connection.close()
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    store_or_update_password(username, password)
+    print(f"Password for user '{username}' has been stored or updated.")
