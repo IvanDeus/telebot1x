7dbde50ffa5e7fa929455f0fe8f5127e373a7f4a
Ubuntu <ubuntu@ip-172-31-42-165.us-east-2.compute.internal>
2023-10-26 15:12:53 +0000

    cfg example file added

---
+++ telebot-hook1x-cfg-example.php
@@ -0,0 +1,13 @@
+<?php
+// Ivan Deus telebot cfg
+// Replace with your MySQL database credentials
+    $db_host = 'localhost';
+    $db_username = 'xxx';
+    $db_password = 'xxx';
+    $db_name = 'xxx';
+ // Replace with admin name
+    $admin_name='ivan';
+ // Replace with your Telegram bot token
+    $bot_token = 'xxx:xxx';
+// end of file 
+?>
