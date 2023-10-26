894809fa655b1fee0f8d5a611148451e54e8eeb0
Ubuntu <ubuntu@ip-172-31-42-165.us-east-2.compute.internal>
2023-10-26 14:32:59 +0000

    new file:   .gitignore 	new file:   telebot-h-files/mini.jpg 	modified:   telebot-hook1x-cfg.php

--- telebot-hook1x-cfg.php
+++ telebot-hook1x-cfg.php
@@ -1,16 +1,5 @@
 <?php
 // Ivan Deus telebot cfg
-// DB construction, copy this to MySQL to create a new table
-/*
-CREATE TABLE telebot_users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    chat_id VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    lastupd TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    lastmsg TEXT );
-
-*/
-
 // Replace with your MySQL database credentials
     $db_host = 'localhost';
     $db_username = 'xxx';
@@ -20,8 +9,5 @@ CREATE TABLE telebot_users (
     $admin_name='ivan';
  // Replace with your Telegram bot token
     $bot_token = 'xxx:xxx';
- // filenames to send 
- $imgtosend = 'Asya-B-aa250.jpg';
- $pdftosend = 'guide.pdf';
 //eof
 ?>
