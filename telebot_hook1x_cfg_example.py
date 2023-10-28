67122e18c00bcae2fa2093d6776f9b1d8d12f967
Ubuntu <ubuntu@ip-172-31-42-165.us-east-2.compute.internal>
2023-10-28 03:41:27 +0000

    sql update

--- telebot_hook1x_cfg_example.py
+++ telebot_hook1x_cfg_example.py
@@ -4,7 +4,6 @@
 '''
 -- MySQL dump 10.13  Distrib 8.0.34, for Linux (x86_64)
 -- ------------------------------------------------------
--- Server version       8.0.34-0ubuntu0.22.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -36,7 +35,6 @@ CREATE TABLE `telebot_admins` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
 -- Table structure for table `telebot_chats`
 --
 
@@ -54,7 +52,6 @@ CREATE TABLE `telebot_chats` (
 ) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
 -- Table structure for table `telebot_sched`
 --
 
@@ -79,7 +76,7 @@ CREATE TABLE `telebot_sched` (
 
 LOCK TABLES `telebot_sched` WRITE;
 /*!40000 ALTER TABLE `telebot_sched` DISABLE KEYS */;
-INSERT INTO `telebot_sched` VALUES (1,1,'mini.jpg','This is a first message from series of scheduled messages from bot','Help, /help','2023-10-20 00:00:00',1),(2,4,'mini.jpg','Should you stay or should you go?','Subscribe, /sub, Unsubscribe, /unsub, Call manager, /manager','2023-10-20 00:00:00',1),(3,24,'mini.jpg','If you need human touch please call our manager','Help, /help, Call manager, /manager','2023-10-20 00:00:00',1),(4,6,'mini.jpg','Юные и почтенные эмансипе должны знать об изобретениях, их освободивших. Как раз о таковом вам расскажет Ася. Вы узнаете:\r\n\r\n- что общего у микрофона и презервативов?\r\n- как изобретение микрофона повлияло на вокал?\r\n','None','2023-10-20 00:00:00',1),(5,60,'mini.jpg','Зачем нужна РАСПЕВКА? Расскажет #Асяпишет\r\n(Посты от Аси я, Ботесса Сингирелла, отмечаю под хэштегом #АсяПишет )\r\n\r\nВы, наверняка, слышали о таком понятии как \"распевка\" или \"warm-up\". Прежде, чем исполнитель(ница) начинает репетицию или выступление, он(а) обязательно распевается!\r\n','None','2023-10-20 00:00:00',1),(6,120,'mini.jpg','Доброго дня!\r\n“Желаю научиться импровизировать и впечатлять слушателей своей импровизацией. Как это сделать?” Ася расскажет, как научиться этой науке. #АсяПишет\r\n\r\n','None','2023-10-20 00:00:00',99),(7,120,'mini.jpg','How are you doing today?\r\n','Help, /help, Call manager, /manager','2023-10-20 00:00:00',99),(8,1,'mini.jpg','Give me some feedback please','None','2023-10-20 00:00:00',99),(9,1440,' ','Концерт состоится завтра, 4 ноября в 19 ч 00 м (GMT+3)',' ','2023-11-04 16:00:00',2),(10,45,' ','Концерт начинается через 45 минут!\r\nСсылка на концерт: https://meet.google.com/',' ','2023-11-04 16:00:00',2),(11,5,' ','Концерт уже почти здесь, присоединяйтесь!\r\nСсылка на концерт: https://meet.google.com/',' ','2023-11-04 16:00:00',2),(12,5,'mini_sin.jpg','Концерт окончен, но музыка вечна!\r\nПриходите к нам на вокал!','None','2023-10-04 18:00:00',2);
+INSERT INTO `telebot_sched` VALUES (1,1,'mini.jpg','This is a first message from series of scheduled messages from bot','Help, /help','2023-10-20 00:00:00',1),(2,4,'mini.jpg','Should you stay or should you go?','Subscribe, /sub, Unsubscribe, /unsub, Call manager, /manager','2023-10-20 00:00:00',1),(3,24,'mini.jpg','Please stay with me...','Help, /help, Call manager, /manager','2023-10-20 00:00:00',1),(4,6,'mini.jpg','4th time is the churm!\r\nHey~\r\nHow are you?','Help!, /help','2023-10-20 00:00:00',1),(5,60,'mini.jpg','This is the message number 5','None','2023-10-20 00:00:00',1),(6,120,'mini.jpg','Доброго дня!\r\n“Желаю научиться импровизировать и впечатлять слушателей своей импровизацией. Как это сделать?” Ася расскажет, как научиться этой науке. \'#АсяПишет\'\r\n\r\n','None','2023-10-20 00:00:00',99),(7,120,'mini.jpg','How are you doing today?\r\n','Help, /help, Call manager, /manager','2023-10-20 00:00:00',99),(8,1,'mini.jpg','Give me some feedback please','None','2023-10-20 00:00:00',99),(9,1440,' ','Концерт состоится завтра, 4 ноября в 19 ч 00 м (GMT+3)',' ','2023-11-04 16:00:00',2),(10,45,' ','Концерт начинается через 45 минут!\r\nСсылка на концерт: https://meet.google.com/',' ','2023-11-04 16:00:00',2),(11,5,' ','Концерт уже почти здесь, присоединяйтесь!\r\nСсылка на концерт: https://meet.google.com/',' ','2023-11-04 16:00:00',2),(12,5,'mini_sin.jpg','Концерт окончен, но музыка вечна!\r\nПриходите к нам на вокал!','None','2023-10-04 18:00:00',2);
 /*!40000 ALTER TABLE `telebot_sched` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,16 +113,15 @@ CREATE TABLE `telebot_vars` (
   `param` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   `value` varchar(3800) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `telebot_vars`
 --
 
 LOCK TABLES `telebot_vars` WRITE;
 /*!40000 ALTER TABLE `telebot_vars` DISABLE KEYS */;
-INSERT INTO `telebot_vars` VALUES (1,'imgtosend','Asya-B-aa250.jpg'),(2,'pdftosend','guide.pdf'),(3,'msg1','I do not understand. Press \'help\' for assistance.'),(4,'msg2','Ivan Deus bot welcomes you! To get file press \'guide\' or press \'help\' for all available commands '),(5,'msg3','Super guide! Enjoy! It is the best.'),(6,'msg4','This is a help message. Try /start or \'guide\'. \r\nYou can subscribe with \'sub\' and undo with \'unsub\'.\r\n'),(15,'massmessage1','This is it, you got a fortune! It is your lucky day :)');
+INSERT INTO `telebot_vars` VALUES (1,'imgtosend','Asya-B-aa250.jpg'),(2,'pdftosend','guide.pdf'),(3,'msg1','I do not understand. Press \'help\' for assistance :/\r\nI am truly deeply terribly sorry.'),(4,'msg2','Ivan Deus bot welcomes you! To get file press \'guide\' or press \'help\' for all available commands '),(5,'msg3','Super guide! Enjoy! It is the best.'),(6,'msg4','This is a help message. Try /start or \'guide\'. \r\nYou can subscribe with \'sub\' and undo with \'unsub\'.\r\n'),(15,'massmessage1','This is it, you got a fortune! \r\nIt is your lucky day :)\r\nDon\'t worry, be happy!'),(16,'poll_inform','Thanks for voting!'),(17,'poll_option','Red; Green; Blue'),(18,'poll_question','What is your favorite color?');
 /*!40000 ALTER TABLE `telebot_vars` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -137,6 +133,7 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
 -- Dump completed
  
 '''
