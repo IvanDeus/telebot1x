813035c90f3ce9f449904562ea93c82dce710a5b
IvanDeus <146208093+IvanDeus@users.noreply.github.com>
2023-11-03 05:06:08 +0300

    Update telebot_hook1x_admin_password.py

--- telebot_hook1x_admin_password.py
+++ telebot_hook1x_admin_password.py
@@ -1,11 +1,9 @@
 import pymysql
 from passlib.hash import pbkdf2_sha256
 from telebot_hook1x_cfg import db_host, db_username, db_password, db_name, mysql_unix_socket
-
 # Function to hash a password using pbkdf2_sha256
 def hash_password(password):
     return pbkdf2_sha256.hash(password)
-
 # Function to insert or update a user's password in the database
 def update_password(username, password):
     # Connect to the MySQL database
@@ -17,7 +15,6 @@ def update_password(username, password):
         unix_socket=mysql_unix_socket
     )
     cursor = db.cursor()
-
     # Check if the user exists
     cursor.execute("SELECT id FROM telebot_admins WHERE name = %s", (username,))
     user_id = cursor.fetchone()
@@ -31,18 +28,13 @@ def update_password(username, password):
         # User doesn't exist, insert a new record
         hashed_password = hash_password(password)
         cursor.execute("INSERT INTO telebot_admins (name, passwd) VALUES (%s, %s)", (username, hashed_password))
-
     db.commit()
     db.close()
-
 if __name__ == "__main__":
     import argparse
-
     parser = argparse.ArgumentParser(description="Update user password in the telebot_admins table")
     parser.add_argument("username", type=str, help="Username for the user")
     parser.add_argument("password", type=str, help="Password to be hashed and stored")
-
     args = parser.parse_args()
-
     update_password(args.username, args.password)
     print("Password for user '{}' has been hashed and stored in the database.".format(args.username))
