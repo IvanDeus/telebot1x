18fa12a521d5d02061e2278474d7667dfca861be
IvanDeus <146208093+IvanDeus@users.noreply.github.com>
2024-03-03 09:12:29 +0300

    Create run_telebot1x.py

---
+++ run_telebot1x.py
@@ -0,0 +1,51 @@
+#python3 script to start bot process
+import subprocess
+import sys
+# Config import
+from telebot1x_cfg import *
+# Help message
+if len(sys.argv) == 1:
+    print("Usage: {} [start|stop|status]".format(sys.argv[0]))
+    sys.exit(1)
+# status
+def filter_processes_by_port(bot_lport):
+    # Run the ps aux command to get a list of all processes
+    ps_output = subprocess.check_output(["ps", "aux"])
+    # Split the output into lines
+    ps_lines = ps_output.decode().split('\n')
+    # Filter the lines based on the local port
+    filtered_lines = [line for line in ps_lines if ":{} ".format(bot_lport) in line]    
+    return filtered_lines
+
+# Start, stop, or check status of Gunicorn
+action = sys.argv[1]
+if action == "start":
+    filtered_processes = filter_processes_by_port(bot_lport)
+    if filtered_processes:
+        print ("Singirella is already running.")
+    else:
+        subprocess.Popen(
+        ["gunicorn", "-b", "localhost:{}".format(bot_lport), "-w", "4", "-t", "222", "--log-file={}".format(logfpath), "singirella:app"]
+        )
+        print("Singirella started.")
+elif action == "stop":
+    # Get the PID of the Gunicorn process and kill it
+    try:
+        gunicorn_pid = subprocess.check_output(["pgrep", "-f", "gunicorn"]).splitlines()[0]
+        subprocess.call(["kill", gunicorn_pid.decode()])
+        print("Singirella stopped.")
+    except subprocess.CalledProcessError:
+        print("Singirella is not running.")
+elif action == "status":
+    # Show the status of Gunicorn processes
+    try:
+        filtered_processes = filter_processes_by_port(bot_lport)
+        if filtered_processes:
+            print('\n'.join(filtered_processes))
+        else:
+            print ("Singirella is not running.")
+    except subprocess.CalledProcessError:
+        print("Error while checking Gunicorn status.")
+else:
+    print("Usage: {} [start|stop|status]".format(sys.argv[0]))
+    sys.exit(1)
